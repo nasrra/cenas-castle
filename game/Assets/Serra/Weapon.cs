1b1bb989cc24b10b24314a6943a5738cdf36c63c
nassra <sneakysloth05@gmail.com>
2024-05-17 20:30:55 +1000

    created weaponholder class

---
+++ game/Assets/Serra/Weapon.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Serra{ public abstract class Weapon : MonoBehaviour{
+
+
+
+
+public abstract void action_1();
+public abstract void action_2();
+
+
+
+
+}}
---
+++ game/Assets/Serra/WeaponHolder.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+namespace Serra{ public class WeaponHolder : MonoBehaviour{
+
+
+
+
+[Header("Weapon")]
+[SerializeField] Weapon weapon;
+public event Action
+    action_1_event,
+    action_2_event;
+
+
+
+
+void Start(){
+    link_weapon();
+}
+
+void link_weapon(){
+    action_1_event += weapon.action_1;
+    action_2_event += weapon.action_2;
+}
+
+public void set_weapon(Weapon weapon){
+    unlink_weapon();
+    this.weapon = weapon;
+    link_weapon();
+}
+
+public void use_action_1(){
+    action_1_event?.Invoke();
+}
+
+public void user_action_2(){
+    action_2_event?.Invoke();
+}
+
+void unlink_weapon(){
+    action_1_event -= weapon.action_1;
+    action_2_event -= weapon.action_2;
+}
+
+void OnDestroy(){
+    unlink_weapon();
+}
+
+
+
+
+}}
